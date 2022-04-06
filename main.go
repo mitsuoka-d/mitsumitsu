33dccc2ffb7dc86297843b3344534d1941059fbf
mitsu <d.mitsuoka@newbees.jp>
2022-04-06 12:31:30 +0900

    コミットテスト

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	e := echo.New()  // echo を利用する
     // GET リクエストでパスが `/` のとき第２引数の関数を実行する
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello")
 	})
 
     // 1323 ポートでリッスンを開始。 start がエラーを起こしたら Fatal を起こしてログに記録する
